b1a54c7ff0b6b1d8661eac02e7dc04d932045407
Doug Sparling <dsparling@amuniversal.com>
2013-10-25 13:45:32 -0500

    updated specs to use expect syntax

--- spec/access_token_spec.rb
+++ spec/access_token_spec.rb
@@ -1,28 +1,26 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
 describe "AccessToken" do
 
   before(:each) do
     @t = AccessToken::Token.new('doug','secret','md5')
   end
 
-  it "should require username" do
-    @t.username.should_not be_empty
+  it "require username" do
+    expect(@t.username).not_to be_empty
   end
 
-  it "should require password" do
-    @t.password.should_not be_empty 
+  it "require password" do
+    expect(@t.password).not_to be_empty
   end
 
-  it "should generate valid timestamp" do
-    @t.timestamp.should >= Time.now.to_i
+  it "generate valid timestamp" do
+    expect(@t.timestamp).to be >= Time.now.to_i
   end
 
   it "should generate valid token" do
-    @t.token.length.should == 35 #39
-    @t.validate(@t.token, @t.username, @t.password).should == true
+    expect(@t.token.length).to eq(35)
+    expect(@t.validate(@t.token, @t.username, @t.password)).to eq(true)
   end
 
 end
