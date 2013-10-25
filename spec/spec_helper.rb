abb50432841b85bb4a9f4b5c1a8164f335b7b3d5
Doug Sparling <dsparling@amuniversal.com>
2013-10-25 13:30:14 -0500

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
-begin
-  require 'spec'
-rescue LoadError
-  require 'rubygems'
-  gem 'rspec'
-  require 'spec'
-end
+require 'rspec'
+require 'simplecov'
+SimpleCov.start
+
+# Load in all of our supporting code
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'access_token'
